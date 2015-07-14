cfe63ca19071900776d293701d0852db584f6486
wisaruty <stay_momo@hotmail.com>
2015-07-14 16:02:44 +0700

    modified:   client/templates/posts/post_item.js

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,11 +3,5 @@ Template.postItem.helpers({
         var a = document.createElement('a');
         a.href =this.url;
         return a.hostname;
-    ddd
-    ddd
-    ddd
-    dds
-
-    dsds
     }
 })
