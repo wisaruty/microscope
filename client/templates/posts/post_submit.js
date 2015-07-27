ff351c9c46b11ad41688e881424d133dff23281d
wisaruty <stay_momo@hotmail.com>
2015-07-27 09:54:04 +0700

    new file:   client/helpers/config.js 	new file:   client/templates/includes/access_denied.html 	new file:   client/templates/includes/header.html 	new file:   client/templates/posts/post_submit.html 	new file:   client/templates/posts/post_submit.js 	new file:   lib/collection/posts.js

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,15 @@
+Template.postSubmit.events({
+  'submit form': function (e) {
+    e.preventDefault();
+
+    var post ={
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+    Meteor.call('postInsert',post,function (error,result) {
+      if (error) 
+        return alert(error.reason);
+      Router.go('postPage',{_id: result._id});
+    })
+  }
+});
