b213148328a34b8f59ba058edfc0afb69f3ba55a
wisaruty <stay_momo@hotmail.com>
2015-08-06 10:02:09 +0700

    10.3

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,9 +1,14 @@
 Template.postItem.helpers({
-  ownPost: function() {    return this.userId === Meteor.userId();  },
-
+  ownPost: function() { 
+    return this.userId === Meteor.userId(); 
+  },
   domain: function(){
     var a = document.createElement('a');
     a.href =this.url;
     return a.hostname;
+  },
+  commentsCount: function(){
+    return Comments.find({postId: this._id}).count();
   }
-})
+
+});
