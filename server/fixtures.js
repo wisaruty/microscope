eff923dc65df04ab5e7f38fff654e63e26062867
wisaruty <stay_momo@hotmail.com>
2015-08-06 16:46:39 +0700

    12-2

--- server/fixtures.js
+++ server/fixtures.js
@@ -52,4 +52,22 @@ if (Posts.find().count() === 0) {
         submitted: new Date(now - 12 * 3600 * 1000),
         commentsCount: 0
     });
+    Posts.insert({
+        title: 'The Meteor Book',
+        userId: tom._id,
+        author: tom.profile.name,
+        url: 'http://themeteorbook.com',
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0
+    });
+    for (var i = 0; i < 10; i++) {
+        Posts.insert({
+            title: 'Test post #' + i,
+            author: sacha.profile.name,
+            userId: sacha._id,
+            url: 'http://google.com/?q=test-' + i,
+            submitted: new Date(now - i * 3600 * 1000),
+            commentsCount: 0
+        });
+    }
 }
